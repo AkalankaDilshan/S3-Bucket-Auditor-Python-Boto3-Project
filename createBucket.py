844cff95b214dd3e21ab5eaf329da566ccad590d
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-24 13:16:45 +0530

    refactor: create creta s3 bucket function

--- createBucket.py
+++ createBucket.py
@@ -5,16 +5,28 @@ from botocore.exceptions import ClientError
 s3 = boto3.client('s3')
 response = s3.list_buckets()
 
-#print(response) # this give nested Dictionary, key: value pair
-
-#print(response['Buckets']) # that Dictionary has buckets list
-#print(f" first bucket details: {response['Buckets'][0]}") # here get first item in buckets list, but first item is another dictionary
-#print(f"first bucket name: {response['Buckets'][0]["Name"]}")
-
-# List all the existing buckets
-for i in response['Buckets'] :
-    print(f"{i["Name"]}")
     
 '''Create Bucket'''
-def 
+def create_s3_bucket (bucket_name, region ='us-east-1'):
+    try: 
+        bucket_config = {}
+        s3_client = boto3.client('s3', region_name = region)
+        
+        if region != 'us-east-1':
+            bucket_config['CreateBucketConfiguration'] = {'LocationConstraint': region}
+            
+        # create bucket
+        s3_client.create_bucket(Bucket=bucket_name, **bucket_config)
+        #The ** operator takes a dictionary and 
+        #unpacks it into keyword arguments. It's like telling 
+        #Python: "Take this dictionary and pass
+        #its contents as separate named arguments to the function
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+# Call function
+#create_s3_bucket("akalanka-test-bucket4f45")
+
     
