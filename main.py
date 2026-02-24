82480a7c4cca6cf2beae1c6a7a9ed573df7b40ae
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-24 16:20:25 +0530

    refactor: clearn and organize code

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ for i in bucket_name_list['Buckets']:
 
 # bucket public access or not
 for j in name_list:
-    policy = client.get_public_access_block(
-        Bucket = j
-    )
-    
+    print(f'\n\nBucket name: {j}')
     #encryption
     encryption_rule = client.get_bucket_encryption(
         Bucket = j
     )
-    print(f'\n\nBucket name: {j}')
     print(encryption_rule['ServerSideEncryptionConfiguration'])
+    
     # bucket public access or not
+    policy = client.get_public_access_block(
+        Bucket = j
+    )
     for key, value in policy['PublicAccessBlockConfiguration'].items():
         print(f'{key} : {value}')
         
