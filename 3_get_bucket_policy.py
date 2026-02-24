b485fb7bbb86b066c76173bcf8b862f515aff356
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-24 16:03:43 +0530

    refactor: roughly finalize bucket access public or not part :articulated_lorry:

--- 3_get_bucket_policy.py
+++ 3_get_bucket_policy.py
@@ -18,12 +18,26 @@ response = client.get_bucket_policy(
 policy = json.loads(response['Policy'])
 print(f'{policy}\n\n\n')
 
-# get_bucket_policy_status, public or not
+# get_bucket_policy_status
 
-bucket_status = client.get_bucket_policy_status(
+bucket_policy_status = client.get_bucket_policy_status(
     Bucket= "codepipeline-eu-north-1-3350963aa07b-4ea1-9a94-926cfb3be811",
 )
 
-print(bucket_status,"\n") # get full policy status
-print(f'{bucket_status['PolicyStatus']}\n') # get public access policy statment
-print(f'{bucket_status['PolicyStatus']['IsPublic']}\n') ## get full clear answer
+print(bucket_policy_status,"\n") # get full policy status
+print(f'{bucket_policy_status['PolicyStatus']}\n') # get public access policy statment
+print(f'{bucket_policy_status['PolicyStatus']['IsPublic']}\n') ## get full clear answer
+## i feel above one not necessary
+
+# Retrieves the PublicAccessBlock configuration for S3
+s3_public_access_policy = client.get_public_access_block(
+    Bucket='codepipeline-eu-north-1-3350963aa07b-4ea1-9a94-926cfb3be811',
+    ExpectedBucketOwner='375353319115' #account id
+)
+
+print(s3_public_access_policy,"\n\n\n") # all thing with meta data
+print(f'{s3_public_access_policy['PublicAccessBlockConfiguration']}\n\n')
+
+## good method
+for key, value in s3_public_access_policy['PublicAccessBlockConfiguration'].items():
+    print(f'{key} : {value}\n') # .items is dictionary method, it seperate each item to tuples.
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import boto3
 
+client = boto3.client('s3')
+
+## get all s3 bucket name and store it on stack
+## create list
+name_list = []
+
+bucket_name_list = client.list_buckets()
+
+for i in bucket_name_list['Buckets']:
+    #print(f'{i['Name']}\n\n')
+    name_list.append(i['Name'])
+
 # bucket public access or not
+for j in name_list:
+    policy = client.get_public_access_block(
+        Bucket = j
+    )
+    print(f'\n\nBucket name: {j}')
+    for key, value in policy['PublicAccessBlockConfiguration'].items():
+        print(f'{key} : {value}')
+
 
 # missing encryption
 
