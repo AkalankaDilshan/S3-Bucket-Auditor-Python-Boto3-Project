78b6de1e9c99ad0750fb22ebec31d518841fc4ba
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-25 10:16:08 +0530

    refactor: use .get method to better output

--- 5_bucket_versioning.py
+++ 5_bucket_versioning.py
@@ -3,17 +3,21 @@ import boto3
 client = boto3.client('s3')
 
 response = client.get_bucket_versioning(
-    Bucket='codepipeline-eu-north-1-3350963aa07b-4ea1-9a94-926cfb3be811',
+    Bucket='akalanka-test-bucket4f45',
     ExpectedBucketOwner='375353319115' #account id
 )
 
 print(response) # full one
 
-status = response.get('Status')
+status = response.get('Status', 'Disabled')
+### The Anatomy of .get()
+#In Python, you can access dictionary values in two ways:
+#Square Brackets: response['Status'] — Risky.
+#The .get() Method: response.get('Status', 'Disabled') — Safe.
+
+#The syntax follows this logic: dictionary.get(key, default_value)
+
 print(status) # here we clearly see versioning enable or not
 
-if status == 'Enabled':
-    print(status)
-else:
-    print("Versioning is NOT ENABLED")
+
     
